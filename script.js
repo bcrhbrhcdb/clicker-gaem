1ba0505f488457cb8cdeab6cd2f305853aef61ec
bcrhbrhcdb <apet2804@mpsedu.org>
2024-09-11 12:10:16 -0500

    a

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello!');
+
+const amountOfClicks = document.getElementById("clicks");
+const click = document.getElementById("click")
+let clickAmount = 0;
+let totalClicks = 0;
+let amountPerClick = 1;
+//add clicks!!111
+click.addEventListener('click', ()=>{
+    clickAmount += amountPerClick
+    totalClicks += amountPerClick
+    amountOfClicks.textContent = clickAmount;
+    console.log(`Amount of clicks: ${clickAmount} | Total amount of clicks: ${totalClicks}`);
+});
+
+if(totalClicks = 30){
+    upgrade1.style.display = "block"
+}
